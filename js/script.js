956fadd7813ab00a9b129b7c0cf951b100d3fb97
Manikya2004 <163899228+Manikya2004@users.noreply.github.com>
2024-04-09 08:14:15 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -29,15 +29,4 @@ function validateEmail(email) {
     return emailRegex.test(email);
 }
 
-window.onscroll = function() {myFunction()};
 
-var header = document.getElementById("myHeader");
-var sticky = header.offsetTop;
-
-function myFunction() {
-    if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
-    } else {
-        header.classList.remove("sticky");
-    }
-}
